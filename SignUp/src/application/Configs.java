3c35d2673a0b2cc2beba01d83546d43089d07d1a
Maria <manya1999m09@yandex.ru>
2019-06-17 14:52:43 +0300

    message

--- SignUp/src/application/Configs.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-public class Configs {
-protected String dbHost = "localhost";
-protected String dbPort = "3306";
-protected static String dbUser = "root";
-protected static String dbPass = "12345";
-protected String dbName = "testvideo";
-}
